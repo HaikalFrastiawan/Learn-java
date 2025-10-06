72facf3255c3657eabb908494d7ef712c2c2227f
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-10-07 06:01:33 +0700

    06 - Pengenalan Print dan Alur Eksekusi - 2

--- Kelas Terbuka/06 - print dan excetue/src/com/tutorial/Main.java
+++ Kelas Terbuka/06 - print dan excetue/src/com/tutorial/Main.java
@@ -3,6 +3,16 @@ package com.tutorial;
 public class Main {
 
      public static void main(String[] args) {
-        System.out.println("haiii ini hello world");
+        System.out.println("hello teman-teman");
+        System.out.println("baris ke-2 : hai juga akang pukis");
+        System.out.print("baris ke-3 : ini adalah baris ketiga");
+        System.out.print("baris ke-4 : saya meneruskan baris ketiga");
+        System.out.print("baris ke-5 : hallo mahmuy???");
+        System.out.println("baris ke-6 : hallo juga hobloh!!!");
+        System.out.print("baris ke-7 : hai bro, mari kita kongkow\n");
+        System.out.print("baris ke-8 : kongkow kemana brow");
+        System.out.print("baris ke-9 : kita kongkow dirumah\n");
+
+        System.out.printf("wiro sablem %d", 212);
      }
 }
