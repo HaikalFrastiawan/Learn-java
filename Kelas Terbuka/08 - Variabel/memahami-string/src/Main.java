201dd2303ebe92d68b4cb730233dcdbdd6be6a62
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-10-09 06:57:02 +0700

    08 -  Variabel, Assignment dan Deklarasi

---
+++ Kelas Terbuka/08 - Variabel/memahami-string/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+      
+       int a = 10;
+       System.out.println("Nilai a = " + a);
+
+       a = 20;
+         System.out.println("Nilai a setelah diubah = " + a);
+
+        //deklarasi 
+        int b;
+
+        b= 15;
+        System.out.println("Nilai b = " + b);
+        
+
+    }
+}
