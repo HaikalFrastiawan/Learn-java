8351e05c6f0bacee8212556e0b15d6dfb5566fbf
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-07-13 07:06:29 +0700

    OOP - Default Method

--- Programmer-Zaman-Now/OOP/src/Haikal/data/Car.java
+++ Programmer-Zaman-Now/OOP/src/Haikal/data/Car.java
@@ -5,4 +5,10 @@ public interface Car extends HashBrand,IsMaintentance  {
     void drive();
 
     int getTier();
+
+    //default method
+    default boolean isBig() {
+        return false;
+    }
+
 }
