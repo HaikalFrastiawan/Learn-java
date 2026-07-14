aa4e799406f73128c83cfe3ed0ee6fd47bbd667b
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-07-14 12:43:55 +0700

    OOP - Final Method

---
+++ Programmer-Zaman-Now/OOP/src/Haikal/data/SocialMedia.java
@@ -0,0 +1,12 @@
+package Haikal.data;
+
+ class SocialMedia {
+     String name;
+}
+
+//tidak bisa di turunkan
+final class Facebook extends SocialMedia {
+
+}
+
+//class FakeFacebook extends Facebook{} //error
